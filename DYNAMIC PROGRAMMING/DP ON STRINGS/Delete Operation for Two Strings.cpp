37048fc95fc847e92f1a91eae9d97c467dcd506c
Binayak Panda <pbinayak604@gmail.com>
2025-08-15 11:56:23 +0530

    Few problems on DP

---
+++ DYNAMIC PROGRAMMING/DP ON STRINGS/Delete Operation for Two Strings.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+public:
+    int f(string &s1, string &s2)
+    {
+        int n = s1.size();
+        int m = s2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int j = 0; j <= m; j++)
+        {
+            dp[0][j] = 0;
+        }
+        for (int i = 0; i <= n; i++)
+        {
+            dp[i][0] = 0;
+        }
+        // int ans=0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (s1[i - 1] == s2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    int minDistance(string word1, string word2)
+    {
+        int n = word1.size();
+        int m = word2.size();
+        int lcs = f(word1, word2);
+        return (n + m - (2 * lcs));
+    }
+};
