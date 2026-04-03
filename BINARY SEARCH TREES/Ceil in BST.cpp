d07ff3e52f0aada2b34ce317f815f758c569cede
Binayak Panda <pbinayak604@gmail.com>
2026-04-03 12:42:15 +0530

    bst 3

---
+++ BINARY SEARCH TREES/Ceil in BST.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int findCeil(Node* root, int x) {
+        // code here
+        int ans=-1;
+        while(root){
+            if(root->data == x){
+                return x;
+            }else if(root->data > x){
+                ans=root->data;
+                root=root->left;
+            }else{
+                root=root->right;
+            }
+        }
+        return ans;
+    }
+};
